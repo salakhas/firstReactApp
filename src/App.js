51180b7fd0248e4cde95fdc0b4a61348c4636c4b
Salakha S <ssalakha16@gmail.com>
2022-03-03 19:53:55 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let arr1 =["Android","Blackberry","iPhone","Windows Phone"];
+  let arr2 =["Samsung","HTC","Micromax","Apple"];
+  const data = [
+    {title: "Mobile Operating System", subjects:arr1},
+    {title: "Mobile Manufacturers", subjects:arr2}
+]
+  //return <div>{data.map((user) => userComponent(user))}</div>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+  <div>
+    <div className='App blueish'><h1>REACT MOBILE OS</h1></div>
+    <div className='lilSpaceLeft'>
+    {
+      data.map((user) => (<UserComponent title={user.title} subject={user.subjects}/>))
+    }
+    </div>
+  </div>)
+}
+
+function UserComponent({title,subject}){
+  const numbers = [1,2,3,4,5];
+  const number = ['a','b','c','d','e'];
+  return (
+    <div>
+      <h1>{title}</h1>
+      <ul key={title.toString()}>
+        {subject.map((e) => (
+          <li key={e.toString()}>{e}</li>
+        ))}
+      </ul>
     </div>
   );
 }
